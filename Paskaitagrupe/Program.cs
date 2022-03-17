f8921d9109a9b34712bc700667ff060c8ffd3f39
Remusankit <100715395+Remusankit@users.noreply.github.com>
2022-03-17 20:52:38 +0200

    prog

--- Paskaitagrupe/Program.cs
+++ Paskaitagrupe/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace Bendras_projektas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spindulys = 6371;
+            double plotas = 3.14 * Math.Pow(2, spindulys);
+            Console.WriteLine($plotas);
+
+            int spindulys2 = 6371;
+            double rutulioTuris = 1.3 * 3.14 * Math.Pow(3, spindulys2);
+            Console.WriteLine(rutulioTuris);
+
+
+        }
+    }
+}
